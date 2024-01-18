0382ce448984545eba91d46f9dce882b3154a66b
ying <huaiy1105@gmail.com>
2024-01-18 16:07:15 +0000

    implement images

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,8 +1,16 @@
 import castle from '../assets/image/Castle.png'
+import circle from '../assets/image/Exclude.png'
+import pipeman from '../assets/image/Pipeman.png'
+import purple from '../assets/image/Purple.png'
+
 function Header () {
   return (
     <div>
     <img src={castle} alt={castle} />
+    <img src={purple} alt={purple} />
+    <img src={circle} alt={circle} />
+    <img src={pipeman} alt={pipeman}/>
+
     </div>
   )
 }
