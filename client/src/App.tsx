0e149fa652df6369dd5b538ed56a994ea0a70aa3
ying <huaiy1105@gmail.com>
2024-01-21 16:05:57 +0000

    set up tailwind but not working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@
 import HomePage from './components/HomePage'
 import './App.css'
 
+
 function App() {
   
   return (
@@ -9,6 +10,7 @@ function App() {
     <div>
       <HomePage/>
     </div>
+    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
     </>
   )
 }
