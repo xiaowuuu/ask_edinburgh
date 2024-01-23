192fc51a52197de6e19e0701018ec7ee5954ae91
ying <huaiy1105@gmail.com>
2024-01-23 16:33:08 +0000

    delete search button component and add it to search component

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,5 +1,6 @@
-import SearchButton from "./SearchButton";
+import React, { useState } from "react";
 function Search () {
+
   return (
     <>
     <input 
@@ -7,7 +8,7 @@ function Search () {
     className="w-2/5 border border-gray-500 rounded-full mr-4 px-3 py-2 focus:outline-none focus:border-blue-700"
     placeholder="Search your idea!"
     ></input>
-    <SearchButton/>
+    <button type="button" className="text-white bg-purple-700 border  rounded-full mr-4 px-3 py-2">Search With AI</button>
     </>
   )
 }
--- client/src/components/SearchButton.tsx
+++
@@ -1,8 +0,0 @@
-function SearchButton (){
-  // const SearchButton
-  return (
-    <button type="button" className="text-white bg-purple-700 border  rounded-full mr-4 px-3 py-2">Search With AI</button>
-  )
-
-}
-export default SearchButton;
