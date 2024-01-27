ced9179979231ac7f838adcb1464398be36309dd
ying <huaiy1105@gmail.com>
2024-01-27 23:31:25 +0000

    history component can store question now

--- client/src/components/HistoryPage/History.tsx
+++ client/src/components/HistoryPage/History.tsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
-
-function History() {
-  
+// import { useLocation} from 'react-router-dom';
+// import { useState } from 'react';
+interface HistoryProps {
+  searchHistory?: string[];
+}
+function History({searchHistory=[]}:HistoryProps) {
   return (
-    
     <div>
       <h3 className='text-6xl font-ABeeZee'>Your search history</h3>
+      {searchHistory.length>0?(
+        <ul>{searchHistory.map((item, index)=> (
+          <li key={index}>{item}</li>
+        ))}
+          </ul>
+      ):(
+        <p>No search history yet.</p>
+      )}
+      
     </div>
   )
 }
--- client/src/components/HomePage/SearchContainer.tsx
+++ client/src/components/HomePage/SearchContainer.tsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import Search from "./Search";
 import SearchResult from "./SearchResult";
+import History from "../HistoryPage/History";
+
 
 function SearchContainer () {
   const [question, setQuestion] = useState('');
   const [searchClicked, setSearchClicked] = useState<boolean>(false);
+  const [searchHistory, setSearchHistory] = useState<string[]>([]);
   
+  // const location = useLocation();
   const handleQuestionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuestion(event.target.value);
   }
   const handleSearchClick = () => {
     setSearchClicked(true);
     // fetchImage(question);
+    if (question.trim()!== ''){
+      setSearchHistory((prevHistory)=> [...prevHistory,question]);
+    }
   }
   return (
     <div>
@@ -20,7 +27,12 @@ function SearchContainer () {
       onQuestionChange={handleQuestionChange}
       onSearchClick={handleSearchClick}
       />
-      {searchClicked && <SearchResult question={question} searchClicked={searchClicked}/>}
+      {searchClicked 
+      && <SearchResult question={question} searchClicked={searchClicked}/>}
+      <History searchHistory={searchHistory}/>
+
+      
+      
     </div>
   )
 }
