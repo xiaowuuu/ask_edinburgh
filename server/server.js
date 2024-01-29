83860c727fa55853ad6d8515dc3e38cea159e200
ying <huaiy1105@gmail.com>
2024-01-29 13:38:19 +0000

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-app.get('/', function(req, res){
-  res.send('hello')
-})
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+const MongoClient = require('mongodb').MongoClient;
+// const createRouter = require('.')
 
 app.listen(9000, function(){
   console.log('app running on port 9000')
