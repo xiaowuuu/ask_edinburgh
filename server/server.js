e882776e46fdc419fd2f7b9f532a196fa4175f7e
ying <huaiy1105@gmail.com>
2024-01-29 13:10:29 +0000

    setup gitignore

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+app.get('/', function(req, res){
+  res.send('hello')
+})
+
+app.listen(9000, function(){
+  console.log('app running on port 9000')
+})
