54acaf1d47dfcb4d68b834827ee57efcbecf18e8
ying <huaiy1105@gmail.com>
2024-01-30 23:21:21 +0000

    update server.ts

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,23 @@
-const express = require('express');
-const app = express();
-const cors = require('cors')
+import express, { Express } from 'express';
+import cors from 'cors';
+import {MongoClient} from 'mongodb';
+import createRouter from './helpers/create_router';
+
+const app: Express = express();
+const port: number = 9000;
 
 app.use(cors());
 app.use(express.json());
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
 
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true})
-  .then((client) => {
+MongoClient.connect('mongodb://127.0.0.1:27017')
+  .then((client)=> {
     const db = client.db('ask_edinburgh');
     const userCollection = db.collection('user');
     const userRouter = createRouter(userCollection);
     app.use('/api/user', userRouter);
-    
   })
-    .catch(console.error);
+  .catch(console.error);
 
-
-
-app.listen(9000, function () {
-	console.log(`Listening on port ${this.address().port}`);
+app.listen(port, ()=>{
+  console.log(`listening on port ${port}`);
 });
