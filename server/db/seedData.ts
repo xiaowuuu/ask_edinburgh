d32569d7059bf121178cf12c03e09ce999b49604
ying <huaiy1105@gmail.com>
2024-01-31 21:29:37 +0000

    fix seeddata, insert question 2

--- server/db/seedData.ts
+++ server/db/seedData.ts
@@ -1,19 +1,20 @@
 import { ObjectId } from "mongodb";
 const userId = new ObjectId();
-const questionId = new ObjectId();
+const questionId1 = new ObjectId();
+const questionId2 = new ObjectId(); //insert the question 2
 
 const seedData = {
   users: [
     {_id: userId, username: 'user'}
   ],
   questions: [
-    {_id: questionId, userId, questionText:"Is summer the best season of Edinburgh?"},
-    {_id: questionId, userId, questionText:"What are some hidden gems or lesser-known places to visit in Edinburgh that you would recommend to someone visiting for the first time?"},
+    {_id: questionId1, userId, questionText:"Is summer the best season of Edinburgh?"},
+    {_id: questionId2, userId, questionText:"What are some hidden gems or lesser-known places to visit in Edinburgh that you would recommend to someone visiting for the first time?"},
 
   ],
   answers: [
-    {userId, questionId, answerText:"of course"},
-    {userId, questionId, answerText:"One lesser-known gem in Edinburgh is the Dean Village. Tucked away along the Water of Leith, this picturesque village offers a tranquil escape from the bustling city. Stroll along the riverside, admire the historic architecture, and discover a peaceful side of Edinburgh that many tourists might overlook."}
+    {userId, questionId:questionId1, answerText:"of course"},
+    {userId, questionId:questionId2, answerText:"One lesser-known gem in Edinburgh is the Dean Village. Tucked away along the Water of Leith, this picturesque village offers a tranquil escape from the bustling city. Stroll along the riverside, admire the historic architecture, and discover a peaceful side of Edinburgh that many tourists might overlook."}
   ]
 }
 
