a730fa9a5b097902a2aedf02fdca995de974f87f
ying <huaiy1105@gmail.com>
2024-01-31 00:11:38 +0000

    fix question and answer id, next will work on fix can not seed more questions and answers

--- server/db/seedData.ts
+++ server/db/seedData.ts
@@ -1,13 +1,19 @@
 import { ObjectId } from "mongodb";
+const userId = new ObjectId();
+const questionId = new ObjectId();
+
 const seedData = {
   users: [
-    {username: 'user'}
+    {_id: userId, username: 'user'}
   ],
   questions: [
-    {userId: new ObjectId(), questionText:"Is summer the best season of Edinburgh?"}
+    {_id: questionId, userId, questionText:"Is summer the best season of Edinburgh?"},
+    {_id: questionId, userId, questionText:"What are some hidden gems or lesser-known places to visit in Edinburgh that you would recommend to someone visiting for the first time?"},
+
   ],
   answers: [
-    {userId: new ObjectId(), questionId: new ObjectId(), answerText:"of course"}
+    {userId, questionId, answerText:"of course"},
+    {userId, questionId, answerText:"One lesser-known gem in Edinburgh is the Dean Village. Tucked away along the Water of Leith, this picturesque village offers a tranquil escape from the bustling city. Stroll along the riverside, admire the historic architecture, and discover a peaceful side of Edinburgh that many tourists might overlook."}
   ]
 }
 
