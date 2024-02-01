b86deed196eae365ed1c5f50078e36e0c79ab940
ying <huaiy1105@gmail.com>
2024-02-01 22:37:22 +0000

    fix user service to connect with backend, there's an error property does not exist on type never.ts, i found a solution by using react function component to avoid it

--- client/src/components/UserService.tsx
+++ client/src/components/UserService.tsx
@@ -1,23 +1,64 @@
-// import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// const baseUrl = 'http://localhost:9000/api/user'
+const baseUrl = 'http://localhost:9000/'
 
-// const User = () => {
-//   const [user, setUser] = useState([]);
-//   useEffect (() => {
-//     fetchUser();
-//   },[])
-//   const fetchUser = () => {
-//     fetch(baseUrl)
-//     .then((res) => res.json())
-//         .then((data) => setUser(data))
-//         .catch((error) => {
-//           console.error('Error fetching user:', error);
-//         })
-//   }
-//   return (
-//     <>
-    
-//     </>
-//   )
-// }
+type UserData = {
+  user: {
+    _id: string;
+    username: string;
+  };
+  questions: {
+    question: {
+      _id: string;
+      questionText: string;
+    };
+    answers: {
+      _id: string;
+      answerText: string;
+    }[];
+  }[];
+}[];
+
+const UserService: React.FC = () => {
+  const [data, setData] = useState<UserData>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(baseUrl);
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('error fetching data:', error);
+      }
+    }
+    fetchData();
+    }, [])
+  
+  return (
+    <div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.user._id}>
+            User: {item.user.username}
+            <ul>
+              {item.questions.map((question) => (
+                <li key={question.question._id}>
+                  Question: {question.question.questionText}
+                  <ul>
+                    {question.answers.map((answer) => (
+                      <li key={answer._id}>
+                        Answer: {answer.answerText}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default UserService;
