ddb2756696a2d8ff9bceaae434cd8ca407d767d9
ying <huaiy1105@gmail.com>
2024-02-01 23:16:44 +0000

    create a api.ts file to maintain the service better, in this file contains functions interact with backend api

---
+++ client/src/services/Api.ts
@@ -0,0 +1,33 @@
+const baseUrl = 'http://localhost:9000/';
+
+type UserData = {
+  user: {
+    _id: string;
+    username: string;
+  };
+  questions: {
+    question: {
+      _id: string;
+      questionText: string;
+    };
+    answers: {
+      _id: string;
+      answerText: string;
+    }[];
+  }[];
+};
+
+const handleFetch = async (endpoint:string): Promise<any> => {
+      try {
+        const response = await fetch(`${baseUrl}/${endpoint}`);
+        const result = await response.json();
+        return result;
+      } catch (error) {
+        console.error('error fetching data:', error);
+        throw error;
+      }
+    }
+    export const fetchUserData = async(): Promise<UserData[]> => {
+      return handleFetch('user');
+    }
+  
