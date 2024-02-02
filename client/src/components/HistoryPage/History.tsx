3dd490d7ebd94d8baea53ab2aff52c50b1fdf794
ying <huaiy1105@gmail.com>
2024-02-02 00:03:22 +0000

    try to render data on history page but it's not working, issue might be the question component, will take a look tomorrow

--- client/src/components/HistoryPage/History.tsx
+++ client/src/components/HistoryPage/History.tsx
@@ -1,5 +1,7 @@
 // import { useLocation} from 'react-router-dom';
 // import { useState } from 'react';
+import QuestionComponent from "../QuestionComponent";
+
 interface HistoryProps {
   searchHistory?: string[];
 }
@@ -9,7 +11,9 @@ function History({searchHistory=[]}:HistoryProps) {
       <h3 className='text-6xl font-ABeeZee'>Your search history</h3>
       {searchHistory.length>0?(
         <ul>{searchHistory.map((item, index)=> (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            <QuestionComponent question={{_id: index.toString(), questionText: item, answers:[]}}/>
+          </li>
         ))}
           </ul>
       ):(
