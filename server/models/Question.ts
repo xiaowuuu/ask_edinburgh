d7e2050cef0ce56bdcd4d9a5666148b1e921b982
ying <huaiy1105@gmail.com>
2024-02-05 14:37:59 +0000

    updated question model

--- server/models/Question.ts
+++ server/models/Question.ts
@@ -1,13 +1,25 @@
 import mongoose, {Document, Schema} from "mongoose";
 
+interface IAnswer {
+  userId: mongoose.Types.ObjectId;
+  answerId: mongoose.Types.ObjectId;
+  answerText: string;
+}
 interface IQuestion extends Document {
   userId: mongoose.Types.ObjectId;
   questionText: string;
+  answers: IAnswer[];
 }
 
+const AnswerSchema = new Schema<IAnswer>({
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true},
+  answerId: {type: Schema.Types.ObjectId, required: true},
+  answerText: {type:String, requered: true},
+})
 const QuestionSchema = new Schema<IQuestion>({
   userId: {type:Schema.Types.ObjectId, ref: "User", required: true},
   questionText: { type: String, required: true},
+  answers: [AnswerSchema],
 })
 
 const Question = mongoose.model<IQuestion>("Question", QuestionSchema)
