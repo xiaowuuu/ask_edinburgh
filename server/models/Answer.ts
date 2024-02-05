3bcdc7b5402a3cedb51f0345493a46e6afb612dd
ying <huaiy1105@gmail.com>
2024-02-05 14:55:10 +0000

    updated answer model

--- server/models/Answer.ts
+++ server/models/Answer.ts
@@ -6,12 +6,12 @@ interface IAnswer extends Document {
   answerText: string;
 }
 
-const answerSchema = new Schema<IAnswer>({
-  userId: {type: Schema.Types.ObjectId, red: "User", required: true},
-  questionId: {type:Schema.Types.ObjectId, red: "Question", required: true},
+const AnswerSchema = new Schema<IAnswer>({
+  userId: {type: Schema.Types.ObjectId, ref: "User", required: true},
+  questionId: {type:Schema.Types.ObjectId, ref: "Question", required: true},
   answerText: {type: String, required: true},
 })
 
-const Answer = mongoose.model<IAnswer>("Answer", answerSchema);
+const Answer = mongoose.model<IAnswer>("Answer", AnswerSchema);
 
 export default Answer;
