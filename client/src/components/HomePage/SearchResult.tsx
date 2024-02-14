19b02c9e915263f7c2e221afd6371ab32b01aba9
ying <huaiy1105@gmail.com>
2024-02-14 20:26:01 +0000

    render random photo in search result each time submit question

--- client/src/components/HomePage/SearchResult.tsx
+++ client/src/components/HomePage/SearchResult.tsx
@@ -1,3 +1,5 @@
+import { useEffect,useState } from "react";
+
 interface SearchResultProps {
   question: string;
   chatGPTResponse: string;
@@ -5,10 +7,21 @@ interface SearchResultProps {
   error: any;
 }
 function SearchResult ({question, chatGPTResponse}:SearchResultProps) {
+  const [randomNumber, setRandomNumber] = useState<number | null>(null);
+  useEffect(()=> {
+    const newRandomNumber = Math.floor(Math.random()*10)+1;
+    setRandomNumber(newRandomNumber);
+  }, [question]);
   return (
     <div className="">
-      <h3>here's your question: {question}</h3>
-      <h3>here's your answer: {chatGPTResponse}</h3>
+      {randomNumber && (
+        <>
+        <img src={`src/assets/banner_images/${randomNumber}.jpeg`} alt={`Image ${randomNumber}`} />
+        <h3>here's your question: {question}</h3>
+        <h3>here's your answer: {chatGPTResponse}</h3>
+        </>
+      )}
+      
 
     </div>
   )
