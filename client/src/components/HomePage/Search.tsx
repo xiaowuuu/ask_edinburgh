0f7441927f499623cc1eb8cce92061634bf4dfee
ying <huaiy1105@gmail.com>
2024-02-15 01:44:00 +0000

    search input and button css

--- client/src/components/HomePage/Search.tsx
+++ client/src/components/HomePage/Search.tsx
@@ -15,21 +15,23 @@ function Search ({ question, onQuestionChange, onSearchSubmit}:SearchProps) {
     }
   }
   return (
-    <>
-    <form onSubmit={handleSearchSubmit}>
+    <div className="flex flex-col items-center ">
+    <form onSubmit={handleSearchSubmit} className="flex items-center">
     <input 
     type="text"
     value={question}
     onChange={onQuestionChange}
-    className="w-2/5 border border-gray-500 rounded-full mr-4 px-3 py-2 focus:outline-none focus:border-blue-700"
+    className="w-[600px] border border-gray-500 rounded-full mr-4 px-3 py-2 focus:outline-none focus:border-blue-700"
     placeholder="Search your idea!"
     ></input>
     <button 
     disabled={isQuestionEmpty}
     type="submit" 
-    className={`text-white bg-indigo-600 border-transparency rounded-full mr-4 px-3 py-2 ${isQuestionEmpty ? 'bg-zinc-400 cursor-not-allowed' : ''}`}>Search With AI</button>
+    className={`text-white bg-indigo-600 w-[177px] border-transparency rounded-full mr-4 px-3 py-2 ${isQuestionEmpty ? 'bg-zinc-400 cursor-not-allowed' : ''}`}>Search With AI
+    </button>
     {/* button is disabled when question is empty */}
     </form>
+    
     <div className="pt-5 pb-5">
     <span className="text-white  bg-indigo-500 border-transparency rounded-full mr-4 px-3 py-2">
 HISTORY
@@ -47,7 +49,7 @@ HISTORY
     EDINBURG’S HOGMANAY
     </span>
     </div>
-    </>
+    </div>
   )
 }
 export default Search;
