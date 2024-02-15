67aa4da6b56b73fba9fd0563f57a7157e9dfd055
ying <huaiy1105@gmail.com>
2024-02-15 23:46:29 +0000

    add padding to input placeholder

--- client/src/components/HomePage/Search.tsx
+++ client/src/components/HomePage/Search.tsx
@@ -21,7 +21,7 @@ function Search ({ question, onQuestionChange, onSearchSubmit}:SearchProps) {
     type="text"
     value={question}
     onChange={onQuestionChange}
-    className="w-[600px] border border-gray-500 rounded-full mr-4 px-3 py-2 focus:outline-none focus:border-blue-700"
+    className="w-[600px] border border-gray-500 rounded-full mr-4 px-3 py-2 px-5 focus:outline-none focus:border-blue-700"
     placeholder="Search your idea!"
     ></input>
     <button 
