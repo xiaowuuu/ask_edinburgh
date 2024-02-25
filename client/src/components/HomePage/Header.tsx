61c4120ab9cefbe2f0ccaf7d47ae645ef41291aa
ying <huaiy1105@gmail.com>
2024-02-25 14:24:06 +0000

    add text center

--- client/src/components/HomePage/Header.tsx
+++ client/src/components/HomePage/Header.tsx
@@ -18,7 +18,7 @@ function Header () {
         <img src={castle} alt={castle} className='lg:h-40 lg:w-40 md:h-10 md:w-10'/>
         <img src={purple} alt={purple} className=' lg:h-10 lg:w-10 md:h-3 md:w-3'/>
       </div>
-    <div className='p-10'>
+    <div className='p-10 text-center'>
     <p className='text-6xl font-ABeeZee mb-8'>The best place to</p>
     <p className='text-6xl font-amatic mb-8'>
     <span className='text-violet-400'>EXPLORE</span>
